b6ed683cf3389536d032289da1a672f653262281
minetaro12 <osawa2003@gmail.com>
2022-04-09 12:49:31 +0000

    Add tcardgen support

---
+++ hugo-build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+for FILE in ./content/posts/*/*/*.md
+do
+  FULLDIR=$(dirname $FILE)
+  DIRNAME=$(basename $FULLDIR)
+  DATE=$(basename `dirname $FULLDIR`)
+  tcardgen \
+    -f ./font \
+    -o ./static/ogp/$DATE-$DIRNAME.png \
+    -t ./ogp.png \
+    -c ./tcardgen.yml \
+    $FILE
+done
+
+TZ='Asia/Tokyo' hugo --gc --minify
